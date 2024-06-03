732185b9f8fe9e5406dca269b1b6bd3ab1fa91c7
Roberto Vetere <Roberto.vetere@gmail.com>
2024-06-03 14:47:29 +0200

    first commit

---
+++ src/test/java/com/demo_h2_database/demo_h2_database/DemoH2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo_h2_database.demo_h2_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoH2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
