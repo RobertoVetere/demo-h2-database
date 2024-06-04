8e143325872afc6c2637d01c81a9e73a77c32415
Roberto Vetere <Roberto.vetere@gmail.com>
2024-06-04 15:24:53 +0200

    ADD Bootstrap inyection on springboot app

--- src/main/java/com/demo_h2_database/DemoH2DatabaseApplication.java
+++ src/main/java/com/demo_h2_database/DemoH2DatabaseApplication.java
@@ -1,5 +1,7 @@
 package com.demo_h2_database;
 
+import com.demo_h2_database.entities.Book;
+import com.demo_h2_database.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoH2DatabaseApplication implements CommandLineRunner {
 
-
+	@Autowired
+	BookRepository bookRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoH2DatabaseApplication.class, args);
 	}
@@ -16,6 +19,14 @@ public class DemoH2DatabaseApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		Book book = new Book("El Guardián", "Lorenzo Lamas");
+		bookRepository.save(book);
+
+		try{
+			System.out.println(bookRepository.findByTitle("El Guardián").get());
+		}catch (Exception e){
+			System.out.println("El libro no existe: " + e.getMessage());
+		}
 
 	}
 }
--- src/main/java/com/demo_h2_database/repositories/BookRepository.java
+++ src/main/java/com/demo_h2_database/repositories/BookRepository.java
@@ -4,6 +4,9 @@ import com.demo_h2_database.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface BookRepositories extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByTitle(String title);
 }
